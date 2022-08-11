56a7daa146249a82e9582d097bb925c0653d0dfe
Sean <sean@albinodrought.com>
2022-08-10 20:32:11 -0700

    feat: beautiful calendar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,254 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <m-calendar :calendar="calendar" />
+
+    <div v-if="parsedSettings.edit" class="editor">
+      <textarea
+        :class="{ 'editor--broken': settingsBad }"
+        v-model="settings"
+      />
+      <button @click="freeze" type="button">Freeze</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import MCalendar from './components/MCalendar.vue';
+
+const moment = require('moment');
+
+const defaultSettings = (now) => ({
+  partialTitle: null,
+  title: null,
+  date: null,
+  edit: true,
+  events: {
+    // Place 1
+    [`${now.format('yyyy-MM')}-03`]: {
+      border: '#FAA6C4',
+      color: '#202225',
+      start: true,
+      text: '🚘',
+    },
+    [`${now.format('yyyy-MM')}-04`]: {
+      border: '#FAA6C4',
+      color: '#202225',
+      ongoing: true,
+      text: '🍻',
+    },
+    [`${now.format('yyyy-MM')}-05`]: {
+      border: '#FAA6C4',
+      color: '#202225',
+      ongoing: true,
+      text: '🍻',
+    },
+    [`${now.format('yyyy-MM')}-06`]: {
+      border: '#FAA6C4',
+      color: '#202225',
+      ongoing: true,
+      text: '🫂',
+    },
+    [`${now.format('yyyy-MM')}-07`]: {
+      border: '#FAA6C4',
+      color: '#202225',
+      end: true,
+      text: '🚘',
+    },
+    // Place 2
+    [`${now.format('yyyy-MM')}-13`]: {
+      border: '#3BA55D',
+      color: '#202225',
+      single: true,
+      text: '🥾',
+    },
+    // Place 3
+    [`${now.format('yyyy-MM')}-16`]: {
+      border: '#5865F2',
+      color: '#202225',
+      start: true,
+      text: '⛴️',
+    },
+    [`${now.format('yyyy-MM')}-17`]: {
+      border: '#5865F2',
+      color: '#202225',
+      ongoing: true,
+      text: '🫂',
+    },
+    [`${now.format('yyyy-MM')}-18`]: {
+      border: '#5865F2',
+      color: '#202225',
+      ongoing: true,
+      text: '🫂',
+    },
+    [`${now.format('yyyy-MM')}-19`]: {
+      border: '#5865F2',
+      color: '#202225',
+      end: true,
+      text: '⛴️',
+    },
+  },
+});
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    MCalendar,
+  },
+  data() {
+    const now = moment();
+
+    let settings = '{}';
+    try {
+      settings = Buffer.from(window.location.hash.replace(/^#/, ''), 'base64').toString('utf8') || '{}';
+      JSON.parse(settings);
+    } catch (ex) {
+      settings = '{}';
+      console.error('Ignored error during user-supplied settings parse', ex);
+    }
+    if (!settings || settings === '{}') {
+      settings = JSON.stringify(defaultSettings(now), null, 2);
+    }
+
+    let lastGoodSettings;
+    let parsedSettings;
+    try {
+      parsedSettings = JSON.parse(settings);
+      if (parsedSettings.date === undefined) {
+        throw new Error('Bad Settings');
+      }
+      lastGoodSettings = settings;
+    } catch (ex) {
+      parsedSettings = {};
+      lastGoodSettings = '{}';
+      console.error('Ignored error during user-supplied settings parse and save', ex);
+    }
+
+    return {
+      now,
+      settings,
+      lastGoodSettings,
+      parsedSettings,
+    };
+  },
+  computed: {
+    settingsBad() {
+      return this.settings !== this.lastGoodSettings;
+    },
+    calendar() {
+      const settings = {
+        ...defaultSettings(this.now),
+        ...this.parsedSettings,
+      };
+
+      if (!settings.date) {
+        settings.date = this.now.format('yyyy-MM-DD');
+      }
+      const time = moment(settings.date);
+
+      if (!settings.title) {
+        settings.title = [
+          settings.partialTitle,
+          time.format('MMMM yyyy'),
+        ].filter((v) => !!v).join(' ');
+      }
+
+      if (!settings.events) {
+        settings.events = {};
+      }
+
+      const data = {
+        title: settings.title,
+        start: time.clone().startOf('month').startOf('week'),
+        end: time.clone().endOf('month').endOf('week'),
+        days: [],
+        weeks: [],
+        activeMonth: time.month(),
+      };
+
+      let lastWeek = data.start.week();
+      let week = [];
+      for (let i = data.start.clone(); !i.isAfter(data.end); i = i.add(1, 'day')) {
+        const date = i.clone();
+        if (date.week() !== lastWeek) {
+          data.weeks.push(week);
+          week = [];
+          lastWeek = date.week();
+        }
+
+        const eventString = date.format('yyyy-MM-DD');
+        if (settings.events[eventString]) {
+          date.event = settings.events[eventString];
+        }
+
+        data.days.push(date);
+        week.push(date);
+      }
+
+      if (week.length > 0) {
+        data.weeks.push(week);
+      }
+
+      return data;
+    },
+  },
+  methods: {
+    freeze() {
+      window.history.pushState(null, null, `#${Buffer.from(this.lastGoodSettings, 'utf8').toString('base64')}`); // save first
+      window.history.pushState(null, null, `#${Buffer.from(this.lastGoodSettings, 'utf8').toString('base64')}`); // save first, intentional
+      this.settings = JSON.stringify({
+        ...this.parsedSettings,
+        edit: false,
+      }); // intentionally minified
+    },
+  },
+  watch: {
+    settings() {
+      try {
+        const parsedSettings = JSON.parse(this.settings);
+        if (parsedSettings.date === undefined) {
+          throw new Error('Bad Settings');
+        }
+        this.parsedSettings = parsedSettings;
+        this.lastGoodSettings = this.settings;
+        window.history.replaceState(null, null, `#${Buffer.from(this.lastGoodSettings, 'utf8').toString('base64')}`);
+      } catch (ex) {
+        // do nothing
+        console.error('Ignored error during user-supplied settings parse on watch', ex);
+      }
+    },
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body {
+  color: #D6D7D8;
+  background-color: #36393F;
+  font-family: sans-serif;
 }
+</style>
+
+<style lang="scss" scoped>
+#app {
+  display: inline; // helps with firefox screenshot taker
+}
+
+.editor {
+  padding-top: 5em;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 800px;
+
+  &--bad {
+    border: 1px solid red;
+  }
+
+  textarea {
+    height: 500px;
+    color: #D6D7D8;
+    background-color: #36393F;
+  }
+}
+
 </style>
---
+++ src/components/MCalendar.vue
@@ -0,0 +1,176 @@
+<template>
+  <table class="calendar">
+    <thead class="title">
+      <tr>
+        <td :colspan="calendar.weeks[0].length">
+          <div class="titlebox">
+            <span></span>
+            <span>{{ calendar.title }}</span>
+            <img crossorigin="anonymous" :src="calendar.logo">
+          </div>
+        </td>
+      </tr>
+    </thead>
+    <thead class="days">
+      <tr>
+        <th v-for="(day, i) in calendar.weeks[0]" :key="i">
+          {{ day.format('ddd') }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(week, i) in calendar.weeks" :key="i">
+        <td
+          v-for="(day, ii) in week"
+          :key="ii"
+          class="day"
+          :class="{ 'day--other-month': day.month() !== calendar.activeMonth }"
+        >
+          <div class="day-container">
+            <span class="date">
+              {{ day.format('D') }}
+            </span>
+            <span
+              v-if="day.event"
+              class="event"
+              :class="{
+                'event--start': day.event.start,
+                'event--ongoing': day.event.ongoing,
+                'event--end': day.event.end,
+                'event--single': day.event.single,
+              }"
+              :style="{ '--event-color': day.event.color, '--event-border': day.event.border }"
+            >
+              {{ day.event.text }}
+            </span>
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'MCalendar',
+  props: {
+    calendar: Object,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap');
+
+.calendar {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #D6D7D8;
+  background-color: #36393F;
+
+  thead {
+    &.title {
+      font-family: 'Open Sans', sans-serif;
+      font-size: x-large;
+      color: #FFFFFF;
+
+      img {
+        max-height: 5em;
+        width: auto;
+      }
+
+      .titlebox {
+        display: grid;
+        grid-template-columns: 1fr 2fr 1fr;
+        grid-template-rows: 1fr;
+        &>*+* {
+          padding-left: 0.5em;
+        }
+      }
+    }
+
+    &.days {
+      text-transform: uppercase;
+      font-size: smaller;
+      color: #A1A4A8;
+      th, td {
+        height: 2em;
+        width: 6em;
+      }
+    }
+  }
+
+  tbody {
+    th, td {
+      height: 6em;
+      width: 6em;
+    }
+  }
+
+  border-collapse: collapse;
+  thead.days, tbody {
+    tr, td, th {
+      border: 1px solid #41464D;
+      padding: 0.5em;
+    }
+  }
+
+  .day {
+    &--other-month {
+      color: rgba(0,0,0,0);
+    }
+  }
+
+  .day-container {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+  .date {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    text-align: center;
+  }
+  .event {
+    background-color: var(--event-color);
+    border: 2px solid;
+    border-color: var(--event-border);
+    position: absolute;
+    height: 1em;
+    padding: 0.5em;
+
+    top: calc(50% - 0.5em);
+    bottom: 50%;
+
+    &--start {
+      text-align: left;
+      border-radius: 1em 0 0 1em;
+      border-right: 0;
+      left: calc(50% - 1em);
+      right: -0.55em;
+    }
+    &--ongoing {
+      text-align: center;
+      border-left: 0;
+      border-right: 0;
+      left: -0.55em;
+      right: -0.55em;
+    }
+    &--end {
+      text-align: right;
+      border-left: 0;
+      border-radius: 0 1em 1em 0;
+      left: -0.55em;
+      right: calc(50% - 1em);
+    }
+    &--single {
+      text-align: center;
+      border-radius: 1em;
+      left: calc(50% - 2em);
+      right: calc(50% - 2em);
+    }
+  }
+}
+</style>
